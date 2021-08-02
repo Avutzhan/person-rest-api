4e178300df54ad9bd32e81defa6c26c45b989270
sd3@vrcode.kz <sd3@vrcode.kz>
2021-08-02 15:42:18 +0600

    last commit

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # person-rest-api
 
+
+|name|version|
+|:---:|:---:|
+|Java| 11|
+|Spring Boot||
+|HikariCP|datasource|
+|Postgresql|db|
+|FlyWay|migrations|
+|Repository Pattern||
+
 https://www.youtube.com/watch?v=vtPkZShrvXQ&t=339s
 
-1 18 20
+Errors
+
+1. SELECT uuid_generate_v4();
+
+Solution 
+
+ ```
+  db=#  CREATE EXTENSION "uuid-ossp";
+  ```
+
+
--- src/main/java/com/example/demo/dao/PersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -1,6 +1,8 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,14 @@ import java.util.UUID;
 
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPesrson(UUID id, Person person) {
         return 0;
@@ -16,12 +26,26 @@ public class PersonDataAccessService implements PersonDao {
 
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+        final String sql = "SELECT id, name FROM person";
+        return jdbcTemplate.query(sql, (resultSet, i) -> new Person(
+                UUID.fromString(resultSet.getString("id")),
+                resultSet.getString("name")
+        ));
     }
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id, name FROM person WHERE id = ?";
+
+        return Optional.ofNullable(
+                jdbcTemplate.queryForObject(
+                        sql,
+                        (resultSet, i) -> new Person(
+                                UUID.fromString(resultSet.getString("id")),
+                                resultSet.getString("name")
+                        ),
+                        id)
+        );
     }
 
     @Override
